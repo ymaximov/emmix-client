f044af821c69d926a8e73904941f10e42251980f
ymaximov <ymaximov@yahoo.com>
2023-09-13 13:05:38 +0300

    3

--- src/pages/purchasing/Purchasing.js
+++ src/pages/purchasing/Purchasing.js
@@ -1,14 +1,81 @@
 import {Layout} from "../layout/Layout";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './purchasing.css'
 import {useNavigate} from "react-router-dom";
-import {clearVendor} from "../../redux/slices/vendorSlice";
+import {clearVendor, setVendor} from "../../redux/slices/vendorSlice";
 import {useDispatch} from "react-redux";
 import {clearOrder, clearSelectedItem, clearPrice, clearWarehouse, clearQuantity, clearDueDate} from "../../redux/slices/purchaseOrderSlice";
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import 'ag-grid-enterprise';
+import {hideLoading, showLoading} from "../../redux/slices/alertsSlice";
+import axios from "axios";
 
 export const Purchasing = () => {
     const navigate = useNavigate()
     const dispatch = useDispatch()
+    const token = JSON.parse(localStorage.getItem('token')).access_token
+    const tenantId = JSON.parse(localStorage.getItem('token')).tenant_id
+    const [PO, setPO] = useState()
+
+    const columnDefs = [
+
+        {
+            headerName: "PO No.",
+            field: "id",
+        },
+        {
+            headerName: "Buyer",
+            field: "user.first_name",
+        },
+        {
+            headerName: "Vendor",
+            field: "vendor.company_name",
+        },
+        {
+            headerName: "Ship-To",
+            field: "warehouse.warehouse_name",
+        },
+        {
+            headerName: "PO Total",
+            field: "total_amount",
+        },
+        {
+            headerName: "Invoiced",
+            field: "invoice_status",
+        },
+    ];
+    const handleCellClicked = (params) => {
+        console.log('AG GRID cell clicked', params);
+        dispatch(setVendor(params.data))
+        navigate('/vendors/vendorprofile')
+    };
+
+    const getPOData = async () => {
+        try {
+            dispatch(showLoading());
+            const res = await axios.get(`/api/purchasing/get-all-po-by-tenant/${tenantId}`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log(res, 'response')
+            dispatch(hideLoading());
+            if (res.status === 200) {
+                console.log(res, 'RES')
+                setPO(res.data.data)
+            }
+        } catch (error) {
+            dispatch(hideLoading());
+            console.log(error)
+        }
+    };
+    const filteredPO = PO?.filter(item => item.status === 'open');
+    // console.log(filteredPO, 'FILTERED')
+    useEffect(() => {
+        getPOData()
+    }, []);
     return (
         <>
         <Layout />
@@ -25,6 +92,12 @@ export const Purchasing = () => {
                         navigate('/purchasing/createpo')}
                     }></i>
                     <i className="ri-search-line ml-1" onClick={''}></i>
+                </div>
+                <h1 className={'mt-2 mb-3'}>Open Purchase Orders</h1>
+                <div>
+                    <div className="ag-theme-alpine" style={{ height: '300px', width: '100%' }}>
+                        <AgGridReact rowData={PO} columnDefs={columnDefs} onCellClicked={handleCellClicked} />
+                    </div>
                 </div>
             </div>
         </>
