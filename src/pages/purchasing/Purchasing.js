d15cc370eb31c7d597712f997c1bc3701962e839
ymaximov <ymaximov@yahoo.com>
2023-09-16 14:05:04 +0300

    4

--- src/pages/purchasing/Purchasing.js
+++ src/pages/purchasing/Purchasing.js
@@ -37,7 +37,8 @@ export const Purchasing = () => {
     const [PO, setPO] = useState()
     const [showSearchPO, setShowSearchPO] = useState(false)
     const openPO = PO?.filter(item => item.status === 'open').length;
-    const notInvoicedPO = PO?.filter(item => !item.invoiced).length;
+    const notInvoicedPO = PO?.filter(item => !item.invoiced && item.status !== 'void').length;
+
     console.log(openPO, 'OPE PO')
     const totalAmountOfNonInvoiced = PO?.filter(item => !item.invoiced).reduce((acc, item) => acc + item.total_amount, 0);
     const openPOByUserID = PO?.filter(item => item.user_id === userID && item.status === 'open');
--- src/pages/purchasing/ViewPO.js
+++ src/pages/purchasing/ViewPO.js
@@ -362,29 +362,8 @@ export const ViewPO = () => {
                     </Col>
 
                     <Col span={8} xs={240} s={24} lg={8}>
-                        <div>
-                            <label htmlFor="warehouse_id" className="block text-sm font-medium leading-6 text-gray-900">
-                                Ship-to Warehouse: {poData.warehouse.warehouse_name}
-                            </label>
-                            {  poData.status !== 'closed' &&  <select
-                                id="warehouse_id"
-                                name="warehouse"
-                                // value={poData.warehouse_id}
-                                onChange={handleWarehouseChange}
-                                required // Add the required attribute
-                                className="block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                            >
-                                <option value=''>Update Warehouse (Select an Option)</option>
-                                {warehouses?.map((wh) => (
-                                    <option key={wh.id} value={wh.id}>
-                                        {wh.warehouse_name}
-                                    </option>
-                                ))}
-                            </select>}
-                            {/* Conditional rendering for error message */}
-                            {/*{purchaseOrder.warehouse === '' && (*/}
-                            {/*    <div className="text-red-500 text-sm mt-1">Warehouse is required</div>*/}
-                            {/*)}*/}
+                        <div className={'font-bold'}>
+                            Ship-to Warehouse: {poData.warehouse.warehouse_name}
                         </div>
 
                         <div>
